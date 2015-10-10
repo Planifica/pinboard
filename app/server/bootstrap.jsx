ca1bfb2190c5b2e94101a89c329f1d43dff767e8
Vlad Barboni <vlad.barboni@gmail.com>
2015-10-10 14:19:31 +0200

    Remove bootstrapping user.

--- app/server/bootstrap.jsx
+++ app/server/bootstrap.jsx
@@ -1,10 +1,4 @@
 Meteor.startup(() => {
-
-    if (Meteor.users.find().count() === 0) {
-        Accounts.createUser({
-            email: "ryancswapp@gmail.com",
-            password: "foodbars"
-        });
-    }
+    
 
 });
