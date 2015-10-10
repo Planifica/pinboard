f91916780b1331e82cb6f88e613dc21a08f13020
Lukas Sporrer <lukas.sporrer@softbricks.de>
2015-10-10 14:45:46 +0200

    eslint fixes

--- app/client/components/layouts/main/MainLayout.jsx
+++ app/client/components/layouts/main/MainLayout.jsx
@@ -1,10 +1,20 @@
 C.MainLayout = React.createClass({
+  mixins: [ReactMeteorData],
+  getMeteorData() {
+      return {
+          currentUser: Meteor.user()
+      }
+  },
     render() {
+      let content = <C.UserLogin />;
+      if(!!this.data.currentUser){
+        content = this.props.content;
+      }
         return (
             <div>
                 {this.props.header}
 
-                {this.props.content}
+                {content}
 
                 {this.props.footer}
             </div>
--- app/client/components/users/UserRegister.jsx
+++ app/client/components/users/UserRegister.jsx
@@ -4,76 +4,77 @@ C.UserRegister = React.createClass({
 
     },
     getInitialState() {
-        return {
-            errors: {}
-        }
+      return {
+          errors: {}
+      }
     },
     getMeteorData() {
-        return {
+      return {
 
-        }
+      }
     },
     onSubmit(event) {
-        event.preventDefault();
+      event.preventDefault()
 
-        var email = $(event.target).find("[name=email]").val();
-        var fullname = $(event.target).find("[name=fullname]").val();
-        var password = $(event.target).find("[name=password]").val();
+      let email = $(event.target).find('[name=email]').val()
+      let fullname = $(event.target).find('[name=fullname]').val()
+      let password = $(event.target).find('[name=password]').val()
 
-        var errors = {};
+      let errors = {}
 
-        if (!email) {
-            errors.email = "Email required"
+      if (!email) {
+        errors.email = 'Email required'
+      }
+
+      if (!fullname) {
+        errors.fullname = 'Fullname required'
+      }
+
+      if (!password) {
+        errors.password = 'Password required'
+      }
+
+      this.setState({
+          errors: errors
+      })
+
+      if (!_.isEmpty(errors)) {
+        return
+      }
+
+      let userOptions = {
+        username: fullname.replace(/ /g, ''),
+        email: email,
+        password: password
+      }
+
+      Accounts.createUser(userOptions, (err) => {
+        if (err) {
+          this.setState({
+              errors: { none: err.reason }
+          })
+        } else {
+          FlowRouter.go('Home')
         }
-
-        if (!fullname) {
-            errors.fullname = "Fullname required"
-        }
-
-        if (!password) {
-            errors.password = "Password required"
-        }
-
-        this.setState({
-            errors: errors
-        });
-
-        if (! _.isEmpty(errors)) {
-            return;
-        }
-
-        let userOptions = {
-          username: fullname.replace(/ /g,''),
-          email: email,
-          password: password
-        }
-
-        Accounts.createUser(userOptions, (err) => {
-            if (err) {
-                this.setState({
-                    errors: {'none': err.reason}
-                });
-
-                return;
-            } else {
-                FlowRouter.go('Home');
-            }
-        });
+      })
     },
     render() {
-        return (
-          <div className="user-login">
-              <div className="user-login-container">
-                  <h1>Register</h1>
-                  <form onSubmit={this.onSubmit} className="u-full-width row">
-                      <C.AuthErrors errors={this.state.errors} />
-                      <C.FormInput hasError={!!this.state.errors.email} name="Email" type="text" label="Email" classExtendInput="u-full-width"/>
-                      <C.FormInput hasError={!!this.state.errors.fullname} name="Fullname" type="text" label="Fullname" classExtendInput="u-full-width"/>
-                      <C.FormInput hasError={!!this.state.errors.password} name="Password" type="password" label="Password" classExtendInput="u-full-width"/>
-                      <button type="submit" className="landing-button u-full-width">{TAPi18n.__('signUp')}</button>
-                  </form>
-              </div>
-          </div>
-        )
+      return (
+        <div className="user-login">
+            <div className="user-login-container">
+                <h1>Register</h1>
+                <form onSubmit={this.onSubmit} className="u-full-width row">
+                    <C.AuthErrors errors={this.state.errors} />
+                    <C.FormInput hasError={!!this.state.errors.email} name="Email" type="text" label="Email"
+                    classExtendInput="u-full-width"/>
+                    <C.FormInput hasError={!!this.state.errors.fullname} name="Fullname" type="text" label="Fullname"
+                    classExtendInput="u-full-width"/>
+                    <C.FormInput hasError={!!this.state.errors.password} name="Password" type="password"
+                    label="Password" classExtendInput="u-full-width"/>
+                    <button type="submit" className="landing-button u-full-width">{TAPi18n.__('signUp')}</button>
+                </form>
+            </div>
+        </div>
+      )
     }
-});
+})
