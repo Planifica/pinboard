011c436dd8186cfd0070bd709e63e57a25a87629
philippspo <philipp.sporrer@planifica.de>
2015-10-11 07:09:41 +0200

    fix note keys

--- app/client/components/boards/Board.jsx
+++ app/client/components/boards/Board.jsx
@@ -61,10 +61,10 @@ C.Board = React.createClass({
     this.renderSideBar(null)
   },
   addNote (position) {
-    this.renderSideBar(<C.NoteDetailView note={ { position } } hideSideBar={this.hideSideBar}/>)
+    this.renderSideBar(<C.NoteDetailView key={position} note={ { position } } hideSideBar={this.hideSideBar}/>)
   },
   showDetailView (note) {
-    this.renderSideBar(<C.NoteDetailView note={note} hideSideBar={this.hideSideBar}/>)
+    this.renderSideBar(<C.NoteDetailView key={ note._id } note={note} hideSideBar={this.hideSideBar}/>)
   },
   renderColumns(numberOfColumns) {
     let columns = []
